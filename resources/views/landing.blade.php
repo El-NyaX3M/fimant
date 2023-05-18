d0e5c3f3c25d5023bad2b93f7a119110d9e3287f
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2023-05-17 21:09:00 -0700

    xd

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,7 +8,7 @@
         <div class="row justify-content-center p-5">
             <div class="col-lg-6 col-xl-7"></div>
             <div class="col-lg-6 col-xl-5">
-                <h1 class="text-black" style="font-size: 5em; font-family: 'Libre Franklin', sans-serif;">Figman´t</h1>
+                <h1 class="text-black" style="font-size: 5em; font-family: 'Libre Franklin', sans-serif;">Figman't</h1>
                 <p class="text-left ps-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti asperiores dolorem vel non saepe commodi eum incidunt perspiciatis consequuntur ipsa.</p>
                 <div class="row">
                     <div class="col-lg-6 col-xl-6"><a href="{{url('login')}}"><button class="btn btn-bd-primary btn-lg fw-bold">Iniciar sesión</button></a></div>
@@ -18,6 +18,6 @@
             </div>
         </div>
     </div>
-    
+    </body>
     
 </x-app-layout>
